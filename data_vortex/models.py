354ed3a7b9a710bc4be685cdceceb91b3f5ee89b
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2024-03-17 15:02:45 +0000

    add models for unit, currenty and price

--- data_vortex/models.py
+++ data_vortex/models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import List
 
 from pydantic import BaseModel, field_validator
@@ -12,10 +13,29 @@ class ListingInfo(BaseModel):
     added_date: str
     phone_number: str
 
-    @field_validator('property_id')
+    @field_validator("property_id")
     @classmethod
     def property_id_is_not_zero(cls, v: str) -> str:
         "Empty listings on rightmove are listed with zero. This is not a valid property id."
-        if v == '0':
-            raise ValueError('ID must not be zero!')
+        if v == "0":
+            raise ValueError("ID must not be zero!")
         return v
+
+
+class PriceUnit(Enum):
+    PER_WEEK = "per_week"
+    PER_MONTH = "per_month"
+    PER_YEAR = "per_year"
+    ONE_OFF = "one_off"
+
+
+class Currency(Enum):
+    GBP = "£"
+    USD = "$"
+    PLN = "zl"
+
+
+class Price(BaseModel):
+    price: str
+    currency: str
+    per: PriceUnit
