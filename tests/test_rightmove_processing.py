bbd6495a47905232cfe71e4a72d16920a7aa426f
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2024-03-17 15:15:50 +0000

    improve formatting

--- tests/test_rightmove_processing.py
+++ tests/test_rightmove_processing.py
@@ -1,7 +1,9 @@
+import datetime
 from pathlib import Path
 
 import pytest
 from bs4 import BeautifulSoup
+from pydantic import HttpUrl
 
 from data_vortex.rightmove_processing import get_listings
 
@@ -17,12 +19,14 @@ def test_get_listings(rightmove_sample: BeautifulSoup) -> None:
     assert len(listings) == 1
     assert listings[0].property_id == "144595010"
     assert listings[0].image_urls == [
-        "https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/260k/259202/144595010/259202_THECI_005196_IMG_00_0000_max_476x317.jpeg",
+        HttpUrl("https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/260k/259202/144595010"
+                "/259202_THECI_005196_IMG_00_0000_max_476x317.jpeg"),
     ]
     assert (
         listings[0].description
-        == "A fully furnished double bedroom is available for couples with balcony in a four-bedroom with one bathroom house-share. This property is conveniently situated in zone 2, providing excellent transportation connections to central London, with easy access to Upper Holloway station nearby."
+        == "A fully furnished double bedroom is available for couples with balcony in a four-bedroom with one "
+           "bathroom house-share. This property is conveniently situated in zone 2, providing excellent "
+           "transportation connections to central London, with easy access to Upper Holloway station nearby."
     )
     assert listings[0].price == "£1,127 pcm"
-    assert listings[0].added_date == "10/02/2024"
-    assert listings[0].phone_number == "020 4538 5940"
+    assert listings[0].added_date == datetime.date(2024, 2, 10)
