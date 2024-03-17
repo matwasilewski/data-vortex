6b85d7bd61465cdc1b845ac8863c7ada2de84b42
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2024-03-17 14:41:35 +0000

    validate ID not to be equal to 0

--- data_vortex/models.py
+++ data_vortex/models.py
@@ -1,8 +1,9 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
+# noinspection PyNestedDecorators
 class ListingInfo(BaseModel):
     property_id: str
     image_urls: List[str]
@@ -10,3 +11,11 @@ class ListingInfo(BaseModel):
     price: str
     added_date: str
     phone_number: str
+
+    @field_validator('property_id')
+    @classmethod
+    def property_id_is_not_zero(cls, v: str) -> str:
+        "Empty listings on rightmove are listed with zero. This is not a valid property id."
+        if v == '0':
+            raise ValueError('ID must not be zero!')
+        return v
