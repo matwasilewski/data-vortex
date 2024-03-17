7eafeec6a99d663e3720157c2b25e60dbd3e7c5c
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2024-03-17 14:41:16 +0000

    update test with correct details of the listing

---
+++ tests/test_rightmove_processing.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+from bs4 import BeautifulSoup
+
+from data_vortex.rightmove_processing import get_listings
+
+
+@pytest.fixture()
+def rightmove_sample(test_resources_root: Path) -> BeautifulSoup:
+    sample_path = test_resources_root / "rightmove_sampe.xml"
+    return BeautifulSoup(sample_path.read_text(), "html.parser")
+
+
+def test_get_listings(rightmove_sample: BeautifulSoup) -> None:
+    listings = get_listings(rightmove_sample)
+    assert len(listings) == 1
+    assert listings[0].property_id == "144595010"
+    assert listings[0].image_urls == [
+        'https://media.rightmove.co.uk:443/dir/crop/10:9-16:9/260k/259202/144595010/259202_THECI_005196_IMG_00_0000_max_476x317.jpeg',
+    ]
+    assert listings[0].description == "A fully furnished double bedroom is available for couples with balcony in a four-bedroom with one bathroom house-share. This property is conveniently situated in zone 2, providing excellent transportation connections to central London, with easy access to Upper Holloway station nearby."
+    assert listings[0].price == "£1,127 pcm"
+    assert listings[0].added_date == "10/02/2024"
+    assert listings[0].phone_number == "020 4538 5940"
