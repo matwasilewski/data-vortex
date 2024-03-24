042d86e95b068213313542df7c4aaa00451e0b2b
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2024-03-24 03:51:18 +0000

    add download raw listings cli option

--- data_vortex/cli.py
+++ data_vortex/cli.py
@@ -20,16 +20,22 @@ def cli():
     "if all current listings are already saved.",
 )
 @click.option(
+    "--download_raw_listings",
+    is_flag=True,
+    default=False,
+    help="Download raw HTML listings to the raw_data directory.",
+)
+@click.option(
     "--wait_time",
     default=0,
     type=float,
 )
-def get_new_properties(continue_search: bool, wait_time: float):
+def get_new_properties(continue_search: bool, download_raw_listings: bool, wait_time: float):
     """
     Fetch and save new rental property listings. If the --continue_search flag is set, the function
     will continue to fetch additional listings even if all current listings are already saved.
     """
-    get_new_listings(continue_search=continue_search, wait_time=wait_time)
+    get_new_listings(continue_search=continue_search, download_raw_listings=download_raw_listings, wait_time=wait_time)
 
 
 cli.add_command(get_new_properties)
