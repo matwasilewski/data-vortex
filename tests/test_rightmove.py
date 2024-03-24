d25630063eb599d31a571a4afd460735dfe0e3c4
mat.p.wasilewski <mat.p.wasilewski@gmail.com>
2024-03-24 01:50:49 +0000

    mock test with full response stored locally

--- tests/test_rightmove.py
+++ tests/test_rightmove.py
@@ -1,3 +1,10 @@
+import pickle
+from pathlib import Path
+
+import pytest
+import requests
+from _pytest.monkeypatch import MonkeyPatch
+
 from data_vortex.rightmove_models import RightmoveRentParams
 from data_vortex.rightmove_processing import (
     get_listings,
@@ -6,7 +13,17 @@ from data_vortex.rightmove_processing import (
 from data_vortex.rightmove_query import search_rental_properties
 
 
-def test_full_run() -> None:
+@pytest.fixture()
+def mock_response(test_resources_root: Path, monkeypatch: MonkeyPatch) -> None:
+    """Requests.get() mocked to return {'mock_key':'mock_response'}."""
+
+    def mock_get(*args, **kwargs):
+        return pickle.load((test_resources_root / "search_response.pkl").open("rb"))
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+
+def test_full_run(mock_response) -> None:
     respo = search_rental_properties(rightmove_params=RightmoveRentParams())
     soup = process_response(respo)
     listings = get_listings(soup)
