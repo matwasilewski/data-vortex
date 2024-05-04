7f2d21233837841f9bb53b3de288de96d9d811d9
mateusz.wasilewski2 <mateusz.wasilewski@biorelate.com>
2024-05-04 17:43:17 +0100

    orm2pydantic conversion script

---
+++ src/data_vortex/utils/conversion.py
@@ -0,0 +1,21 @@
+from pydantic_core import Url
+
+from src.data_vortex.database.models import RentalListing
+from src.data_vortex.rightmove_models import GenericListing, RightmoveRentalListing, Price, Currency, PriceUnit
+
+
+def orm2pydantic_rental_listing(orm_rental_listing: RentalListing) -> RightmoveRentalListing:
+    return RightmoveRentalListing(
+        property_id=orm_rental_listing.property_id,
+        image_url=Url(orm_rental_listing.image_url) if orm_rental_listing.image_url != "None" else None,
+        description=orm_rental_listing.description,
+        price=Price(
+            price=orm_rental_listing.price_amount,
+            currency=Currency[orm_rental_listing.price_currency],
+            per=PriceUnit[orm_rental_listing.price_per],
+        ),
+        added_date=orm_rental_listing.added_date,
+        address=orm_rental_listing.address,
+        postcode=orm_rental_listing.postcode,
+        created_date=orm_rental_listing.created_date,
+    )
