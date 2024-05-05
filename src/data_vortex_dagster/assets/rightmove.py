44fba8374f37529e090f6c1c9a13213ffc1fe414
mateusz.wasilewski2 <mateusz.wasilewski@biorelate.com>
2024-05-05 15:21:25 +0100

    add stub asset for parsed rightmove

--- src/data_vortex_dagster/assets/rightmove.py
+++ src/data_vortex_dagster/assets/rightmove.py
@@ -3,7 +3,8 @@ from typing import List
 from bs4 import BeautifulSoup
 from dagster import AssetExecutionContext, StaticPartitionsDefinition, asset
 
-from data_vortex.rightmove_processing import get_listings
+from src.data_vortex.rightmove_models import GenericListing, RightmoveRentalListing
+from src.data_vortex.rightmove_processing import get_listings, get_detailed_listing
 from src.data_vortex_dagster.resources import ExternalResource
 
 KEY_PREFIX = "ingest_rightmove_backfill"
@@ -21,11 +22,10 @@ STATIC_PARTITIONS_DEF_RIGHTMOVE_BACKFILL = StaticPartitionsDefinition(
     partitions_def=STATIC_PARTITIONS_DEF_RIGHTMOVE_BACKFILL,
 )
 def raw_rightmove(
-    context: AssetExecutionContext,
-    external_resource: ExternalResource,
+        context: AssetExecutionContext,
+        external_resource: ExternalResource,
 ) -> List[bytes]:
     return external_resource.read("source_rigthmove_backfill", context.partition_key)
-
 
 
 @asset(
@@ -34,12 +34,14 @@ def raw_rightmove(
     partitions_def=STATIC_PARTITIONS_DEF_RIGHTMOVE_BACKFILL,
 )
 def parsed_rightmove(
-    context: AssetExecutionContext,
-    raw_rightmove: List[bytes],
-) -> List[bytes]:
+        context: AssetExecutionContext,
+        raw_rightmove: List[bytes],
+) -> List[RightmoveRentalListing]:
+    out = []
 
     for listing_bytes in raw_rightmove:
-        BeautifulSoup(listing_bytes, "xml")
-        nested_listings = get_listings(listing_bytes)
+        bs = BeautifulSoup(listing_bytes.decode("utf-8"), "html.parser")
+        nested_listings = get_detailed_listing(bs)
+        out.append(nested_listings)
 
-    return []
+    return out
